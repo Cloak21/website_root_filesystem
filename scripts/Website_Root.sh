2b9b4204f6122d30c2fe00ad33db999c8f4a7b1b
Cloak21 <52053943+Cloak21@users.noreply.github.com>
2019-07-02 11:20:22 -0700

    Commit Website_Root_filesystem

---
+++ scripts/Website_Root.sh
@@ -0,0 +1,21 @@
+cd desktop
+mkdir Website_root
+echo “Directory named Website_Root is created”
+cd Website_root
+mkdir style images scripts
+echo “Directorys named styles images scripts”
+touch index.html icon.png
+echo “Files named index.html icon.png created”
+cd style
+touch theme.css default.css
+echo “File named theme.css default.css created”
+cd ../
+cd scripts
+touch animation.js
+echo “File named animation.js created”
+cd ../
+cd images
+touch logo.png kirupa.jpeg dancing_banana.gif
+echo “File named  logo.png kirupa.jpeg dancing_ banana.gif created”
+
+
